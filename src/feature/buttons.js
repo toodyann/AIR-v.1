ba845e14dc942e9ec6c3e6b22f4ada666f91e18d
Konovalov Anatolii <mrdenth3@gmail.com>
2025-09-22 16:23:23 +0300

    add first js code

--- src/feature/buttons.js
+++ src/feature/buttons.js
@@ -1,3 +1,17 @@
 export const buttonsClick =() =>{
     console.log('buttonsClick');
 }
+
+console.warn("scss not work");
+
+
+let age = prompt("Склільки тобі")
+if(age > 18){
+    alert("+");
+
+}else{(age < 18)
+    console.log("-");
+}
+
+
+    
